ad6281cdea50c33a7b854ee86a8a25bf643d5bac
Arcmeii <qingxing1055@gmail.com>
2023-04-04 16:42:10 +0800

    修改

--- Python编程：从入门到实践/可视化案例/JSON文件格式/Americas.py
+++ Python编程：从入门到实践/可视化案例/JSON文件格式/Americas.py
@@ -1,11 +1,15 @@
 """
 演示使用Worldmap，突出北美、中美和南美
 """
-
 import pygal
+import pygal_maps_world.maps
 
-wm = pygal.Worldmap()
+wm = pygal_maps_world.maps.World()
+
 wm.title = 'North, Central, and South America'
 
 wm.add("North America", ['ca', 'mx', 'us'])
 wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
+wm.add("South America", ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'fg', 'gy', 'pe', 'py', 'sr', 'uy', 've'])
+
+wm.render_to_file('Americas.svg')
